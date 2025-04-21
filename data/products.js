db93f8b2d8b6bb2acc5e0cdd984835a158f21bfe
BENIE SYLVESTRE <sylvestrebenie7829@gmail.com>
2025-04-21 02:56:14 +0000

    feat: Remplacement de localhost par l URL de mon backend

--- data/products.js
+++ data/products.js
@@ -4,29 +4,31 @@ const productData = [
     name: "Casque Bluetooth",
     description: "Casque audio sans fil avec réduction de bruit.",
     price: 79.99,
-    image: "http://localhost:5000/assets/images/Casque_Bluetooth.jpg",
+    image:
+      "https://checkpoint-ecommerce-backend.onrender.com/assets/images/Casque_Bluetooth.jpg",
   },
   {
     id: "2",
     name: "Clavier mécanique",
     description: "Clavier rétroéclairé RGB avec switchs rouges.",
     price: 99.99,
-    image: "http://localhost:5000/assets/images/Clavier_mecanique.jpg",
+    image:
+      "https://checkpoint-ecommerce-backend.onrender.com/assets/images/Clavier_mecanique.jpg",
   },
   {
     id: "3",
     name: "Souris ergonomique",
     description: "Souris confortable pour les longues sessions.",
     price: 49.99,
-    image: "http://localhost:5000/assets/images/Souris_ergonomique.jpg",
+    image:
+      "https://checkpoint-ecommerce-backend.onrender.com/assets/images/Souris_ergonomique.jpg",
   },
   {
     id: "4",
     name: "Écran 27 pouces",
     description: "Écran 4K UHD ultra-fin avec HDR.",
     price: 299.99,
-    image: "http://localhost:5000/assets/images/ecran_27_pouces.jpg",
+    image:
+      "https://checkpoint-ecommerce-backend.onrender.com/assets/images/ecran_27_pouces.jpg",
   },
 ];
-
-export default productData;
